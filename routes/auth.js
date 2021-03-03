0a3ae08a413da51a88101576645b23b8f432aff5
Michal Raman <raman_michael@hotmail.com>
2021-03-03 21:28:34 +0100

    fixed login problem

--- routes/auth.js
+++ routes/auth.js
@@ -46,23 +46,21 @@ router.post('/signup', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
   passport.authenticate('local', (err, user) => {
     if (err) {
       return res.status(500).json({ message: 'Error while authenticating' });
     }
     if (!user) {
-      return res.status(400).json({ message: 'Wrong credentials' });
+      return res.status(401).json({ message: 'Wrong credentials' });
     }
     req.login(user, err => {
       if (err) {
-        return res
-          .status(500)
-          .json({ message: 'Error while attempting to login' });
+        return res.status(500).json({ message: 'Error while attempting to login' });
       }
-      return res.json(user);
+      res.status(200).json(user);
     });
-  });
+  })(req, res, next);
 });
 
 router.delete('/logout', (req, res) => {
