bd013f1fd7d1fed7d638cd3f935a570ccc648208
Michal Raman <raman_michael@hotmail.com>
2021-03-11 10:37:52 +0100

    deployment

--- app.js
+++ app.js
@@ -97,6 +97,14 @@ app.use("/api", allRoutes);
 
 app.use('/api/auth', require('./routes/auth'));
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
